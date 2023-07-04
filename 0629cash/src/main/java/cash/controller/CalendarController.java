d33b281bdbefe1557aaf07fd440581494dae59cb
electricPo <electricPotatoGarden@gmail.com>
2023-07-04 12:39:13 +0900

    달력에 수입 지출 내역이 표시되도록 코드 추가

--- 0629cash/src/main/java/cash/controller/CalendarController.java
+++ 0629cash/src/main/java/cash/controller/CalendarController.java
@@ -2,6 +2,7 @@ package cash.controller;
 
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,49 +11,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/calendarController")
+import cash.model.CashbookDao;
+import cash.vo.Cashbook;
+
+
+
+@WebServlet("/calendar")
 public class CalendarController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//session 인증 검사
+
+		// session 인증 검사 코드
+		//HttpSession session = request.getSession();
+		//if(session.getAttribute("loginMember") != null) {
+		//	response.sendRedirect(request.getContextPath()+"/cashbook");
+		//	return;
+		//}
+		String memberId = "user1";
 		
+
 		//view에 넘겨줄 달력정보(모델값)
-		//사용자->보고싶은 연도와 월이 넘어온다
-		//int targetYear = Integer.parseInt(request.getParameter("targetYear"));
-		//int targetMonth = Integer.parseInt(request.getParameter("targetMonth"));
-		
-		
-		Calendar firstDay = Calendar.getInstance();
+		Calendar firstDay = Calendar.getInstance(); //오늘 날짜 가져오기
 		/*
 		 * 월 1일을 넘김
 		 * 해당하는 날짜의 요일을 알아냄
 		 */
-		//출력하고자하는 녀도,월,일의 기본값은 이번달 1일
+		//출력하고자하는 년도,월,일의 기본값은 이번달 1일
 		//위 값(츌력하고자하는 년도와 월)이 안 넘어오면 오늘 날짜가 나온다
 		int targetYear = firstDay.get(Calendar.YEAR);
 		int targetMonth = firstDay.get(Calendar.MONTH);
+		
 		//달력출력시 시작 공백수 계산하기 위해 -> 오늘이 무슨 요일인지
 		firstDay.set(Calendar.DATE, 1);
 		
+		//출력하고자 하는 년도, 월이 매개값으로 넘어왔다면
 		if(request.getParameter("targetYear") != null 
 				&& request.getParameter("targetMonth") != null) {
-			targetYear = Integer.parseInt(request.getParameter("targetYear"));
-			targetMonth = Integer.parseInt(request.getParameter("targetMonth"));
 			//api를 통해 Calendar.MONTH에 12가 들어오면 -> 월이 1이 되고 연도는 +1이 된다
 			//api를 통해 Calendar.MONTH에 -1이 들어오면 -> 월이 12이 되고 연도는 -1이 된다
-			firstDay.set(Calendar.YEAR, targetYear);
-			firstDay.set(Calendar.MONTH, targetMonth);
+			firstDay.set(Calendar.YEAR, Integer.parseInt(request.getParameter("targetYear")));
+			firstDay.set(Calendar.MONTH, Integer.parseInt(request.getParameter("targetMonth")));
 		}
-
+		
 		
 
-		//일요일->1, 토요일->7
-		//1일 날짜의 요일을-1		
+		
+		//달력 출력시 시작 공백 개수를 계산
+		//일요일->1 ... 토요일->7	
 		int beginBlank = firstDay.get(Calendar.DAY_OF_WEEK)-1;
 		System.out.println(beginBlank+"<-beginBlank");
 		
-		//출력되는 월의 마지막날짜
+		//출력되는 월의 마지막날짜 가져오기
 		int lastDate = firstDay.getActualMaximum(Calendar.DATE);
 		System.out.println(lastDate+"<-lastDate");
 		
@@ -66,13 +76,18 @@ public class CalendarController extends HttpServlet {
 		System.out.println(totalCell+" <- totalCell");
 		System.out.println(endBlank+" <- endBlank");
 		
+		//모델을 호출해 해당 월의 수입/지출 데이터를 가져온다
+		List<Cashbook>list = new CashbookDao().selectCashbookListByMonth(memberId, targetYear, targetMonth +1);
+		
 		//달력을 출력하는 뷰
 		//넘길 값을 request에 담아서 view 에 넘긴다
 		//묶어서 넘기느냐? 개별적으로 넘기느냐? -> request에 넘길 땐 지금은 개별로 보낸다(가독성)
 		
 		targetYear = firstDay.get(Calendar.YEAR);
 		targetMonth = firstDay.get(Calendar.MONTH);
-
+		
+		
+		//뷰에 값을 전달하기 위해 request 설정하기
 		request.setAttribute("targetYear", targetYear);
 		request.setAttribute("targetMonth", targetMonth);
 		request.setAttribute("beginBlank", beginBlank);
@@ -80,6 +95,10 @@ public class CalendarController extends HttpServlet {
 		request.setAttribute("totalCell", totalCell);
 		request.setAttribute("endBlank", endBlank);
 		
+		request.setAttribute("list", list);
+
+		
+		//달력을 출력하는 뷰로 포워딩하기
 		request.getRequestDispatcher("/WEB-INF/view/calendar.jsp").forward(request, response);
 
 	}
--- 0629cash/src/main/webapp/WEB-INF/view/calendar.jsp
+++ 0629cash/src/main/webapp/WEB-INF/view/calendar.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%><!-- jstl substring호출 -->
 <!-- jsp 컴파일 시 자바코드로 변환되는 c:...(제어문법코드) 커스텀 태그 사용 가능 -->
 <!DOCTYPE html>
 <html>
@@ -24,12 +25,12 @@
 	<h1>
 	${targetYear}년 ${targetMonth+1}월
 	</h1>
-	
+	<!-- 이전달과 다음달로 이동하는 링크를 생성 -->
 	<a href="${pageContext.request.contextPath}/calendarController?targetYear=${targetYear}&targetMonth= ${targetMonth-1}">이전달</a>
-	
 	<a href="${pageContext.request.contextPath}/calendarController?targetYear=${targetYear}&targetMonth= ${targetMonth+1}">다음달</a>
 	
-	<table border="1">
+	<!-- 요일을 표시하는 헤더부분 -->
+	<table border="1" width="80%">
 		<tr>
 			<th>일</th>
 			<th>월</th>
@@ -39,19 +40,42 @@
 			<th>금</th>
 			<th>토</th>
 		</tr>
-		
+
 		<tr>
+			<!-- foreach를 사용해 0부터 totalcell -1까지 반복 var=i로 현재의 값을 저장한다 -->
 			<c:forEach var="i" begin="0" end="${totalCell - 1}" step="1">
-				<c:if test="${i!=0 && i % 7 == 0 }"> <!-- test는 원래 조건이 들어오니까 $ 안 써도 됨 -->
+			<!-- d 변수를 i-beginBlank+1(캘린더에 날짜를 표시하는 값)로 설정 -->
+				<c:set var="d" value="${i-beginBlank+1 }"></c:set> 
+				<!-- 매주마다 행을 나눈다 -->	
+				<c:if test="${i!=0 && i % 7 == 0 }"> 
 					</tr><tr>
 				</c:if>
-				
-				<c:if test="${(i - beginBlank + 1) <1 || (i - beginBlank + 1) > lastDate}">
+				<!-- d값이 유효한 날짜범위에 속하지 않으면 <td>를 생성 -->
+				<c:if test="${d < 1 || d > lastDate}">
 					<td></td>
 				</c:if>
-				
-				<c:if test="${!((i - beginBlank + 1) <1 || (i - beginBlank + 1) > lastDate) }">
-					<td>${i - beginBlank + 1}</td>
+				<!-- else if문은 if문을 !으로  
+					|| choose / otherwise를 쓰면 switch 가능하다-->
+				<c:if test="${!(d <1 || d > lastDate) }">
+					<td>
+				<!-- 유효한 날짜범위에 속한다면 ${d}변수를 사용해 날짜를 출력한다 -->
+						<div>${d}</div>
+				<!-- list 변수에 저장된 객체목록을 c:forEach 를 사용해 반복한다 -->
+						<c:forEach var="c" items="${list}">
+				<!-- 값과 c.getCashbookDate()에서 추출한 날짜가 일치하는 경우 -->
+							<c:if test="${d == fn:substring(c.getCashbookDate(),8,10)}">
+								<div>
+				<!--수입 또는 지출을 표시한다 -->
+									<c:if test="${c.category == '수입'}">
+										<span>+${c.price}</span>
+									</c:if>
+									<c:if test="${c.category == '지출'}">
+										<span style="color:red;">+${c.price}</span>
+									</c:if>
+								</div>
+							</c:if>
+						</c:forEach>
+					</td>
 				</c:if>
 			</c:forEach>
 	</table>
