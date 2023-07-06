b6e0c52563ca83862a7a21754be50e687b919785
electricPo <electricPotatoGarden@gmail.com>
2023-07-06 15:25:22 +0900

    request.getCookies()를 이용해 세션에 저장된 아이디를 일정시간 노출시키는 기능 추가

--- 0629cash/src/main/java/cash/controller/LoginController.java
+++ 0629cash/src/main/java/cash/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,37 +17,77 @@ import cash.vo.Member;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
+	
+	//로그인 폼
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// session 인증 검사 코드
+		//로그인 세션 불러오기
 		HttpSession session = request.getSession();
+		Member member = (Member)session.getAttribute("loginMember");
+		
+		// session 유효성 검사 : 로그인 되어있을 때 cashbook으로 이동
 		if(session.getAttribute("loginMember") != null) {
-			response.sendRedirect(request.getContextPath()+"/cashbook");
+			response.sendRedirect(request.getContextPath()+"/calendar");
 			return;
 		}
+		
+		//쿠키에 저장된 아이디가 있다면 request 속성에 저장
+		Cookie[] cookies = request.getCookies();
+			if(cookies != null) {
+			
+			for(Cookie c : cookies) {
+				if(c.getName().equals("loginMember") == true) {
+					request.setAttribute("loginMember", c.getValue());
+				}
+			}
+		}
+		
+		//로그인 안 되어있을 때 login폼으로 이동
 		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
 	}
+	
+	//로그인 액션
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//인코딩
+		request.setCharacterEncoding("utf-8");
+		
+		//파라미터 값 받아오기
 		String memberId = request.getParameter("memberId");
 		String memberPw = request.getParameter("memberPw");
 		
-		Member member = new Member(memberId, memberPw, null, null);
+		System.out.println("입력id: "+ memberId);
 		
+		//dao 호출
+		Member member = new Member(memberId, memberPw, null, null);
 		MemberDao memberDao = new MemberDao();
 		Member loginMember = memberDao.selectMemberById(member);
-			
-		if(loginMember == null) { // null 로그인실패
+		
+		//로그인 실패시 -> login doGet으로 보냄
+		if(loginMember == null) {
 			System.out.println("로그인 실패");
 			response.sendRedirect(request.getContextPath()+"/login");
 			return;
 		}
 		
-		// 로그인 성공시 : session 사용
+		//로그인 성공시 :cookieId에 저장
+		//로그인 세션저장 + 로그인id를 쿠키(만료기간설정)에 저장 -> loginId로 저장
+		//성공하면 어디로? -> 로그인 성공페이지로 redirect -> / home
+		
+		//idsave 체크값이 넘어왔다면
+		if(request.getParameter("saveId") != null) {
+			Cookie loginIdCookie = new Cookie("loginMember", memberId);
+			loginIdCookie.setMaxAge(60*30);//초*분*시간 -> 30분
+			response.addCookie(loginIdCookie);
+		}
+		
+		// 로그인 성공시 : session에 저장 후 cashbook.jsp으로
 		HttpSession session = request.getSession();
-		System.out.println("로그인 성공");
+		System.out.println("로그인 성공 -> calendar로 이동");
 		session.setAttribute("loginMember", loginMember);
-		response.sendRedirect(request.getContextPath()+"/cashbook");
+		
+		//출력하는 뷰로 포워딩하기
+	  	request.getRequestDispatcher("/WEB-INF/view/cashbook.jsp").forward(request, response);
 	}
 
 }
--- 0629cash/src/main/webapp/WEB-INF/view/login.jsp
+++ 0629cash/src/main/webapp/WEB-INF/view/login.jsp
@@ -8,17 +8,19 @@
 <body>
 	<h1>로그인</h1>
 	<form method="post" action="${pageContext.request.contextPath}/login">
-		<table border="1">
-			<tr>
-				<td>memberId</td>
-				<td><input type="text" name="memberId"></td>
-			</tr>
-			<tr>
-				<td>memberPw</td>
-				<td><input type="password" name="memberPw"></td>
-			</tr>
-		</table>
-		<button type="submit">로그인</button>
+
+		<div>
+			아이디 : <input type="text" name="memberId" value="${loginMember}"></td>
+		</div>
+		<div>
+			비밀번호 : <input type="password" name="memberPw"></td>
+		</div>
+		<div>
+			<input type="checkbox" name="saveId" value="y">ID저장
+		</div>
+		<div>
+			<button type="submit">로그인</button>
+		</div>
 		<a href="${pageContext.request.contextPath}/addMember">회원가입</a>
 	</form>
 </body>
